8781a2145988f8570c7b794b64acea0e72dae5f6
Maximilian Maroe <drakfyre@gmail.com>
2022-01-12 11:37:33 -0800

    There we go!

--- Assets/Scripts/GripScale.cs
+++ Assets/Scripts/GripScale.cs
@@ -18,9 +18,9 @@ public class GripScale : MonoBehaviour
             {
                 float newGripDistance = (worldGrabMovement.leftHand.rigidbody.position - worldGrabMovement.rightHand.rigidbody.position).magnitude;
                 objectToScale.localScale *= newGripDistance/gripDistance;
-                gripDistance = newGripDistance;
             }
-            else if(worldGrabMovement.leftHand.grippedObject && !worldGrabMovement.rightHand.grippedObject)
+
+            if(worldGrabMovement.leftHand.grippedObject && !worldGrabMovement.rightHand.grippedObject)
             {
                 objectToScale = worldGrabMovement.leftHand.grippedObject.transform;
                 gripDistance = (worldGrabMovement.leftHand.rigidbody.position - worldGrabMovement.rightHand.rigidbody.position).magnitude;
@@ -29,6 +29,10 @@ public class GripScale : MonoBehaviour
             {
                 objectToScale = worldGrabMovement.rightHand.grippedObject.transform;
                 gripDistance = (worldGrabMovement.leftHand.rigidbody.position - worldGrabMovement.rightHand.rigidbody.position).magnitude;
+            }
+            else if(worldGrabMovement.rightHand.grippedObject && worldGrabMovement.leftHand.grippedObject)
+            {
+                objectToScale = null;
             }
         }
         else
